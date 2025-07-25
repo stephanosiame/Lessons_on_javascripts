fd2ae6f2d565f94cc65390005d0a115ed9b88204
stephanosiame <siamelstephano@gmail.com>
2025-07-25 14:23:51 +0300

    lesson 4 operator in js

---
+++ module_one/statements/condition_statement/statement_for_condition.js
@@ -0,0 +1,59 @@
+//JavaScript have the conditions statement that is detect the program to do decisions while do the operation.
+
+// if, else if, else and switch case statement
+
+//if 
+/*
+var age = 70;
+
+if (age > 60 ){
+    console.log("group of elder age")
+}
+
+*/
+
+//else if 
+/*
+var days = 'monday'
+
+if (days == "Tuesday"){
+    console.log("This is the second day of the week")
+} else if (days = "monday"){
+    console.log("This is the firstday of the week")
+}else if (days = "sunday"){
+    console.log("This is the seven day of the week")
+
+}
+
+*/
+
+//else
+/*
+var age = 40;
+
+if (age > 60 ){
+    console.log("group of elder age")
+}else{
+    console.log("This Man is so young")
+}
+
+*/
+
+//switch case
+/*
+var days = "Monday";
+
+switch (days){
+    case 'Sunday':
+        console.log("seven day of the week");
+    
+    case "Monday":
+        console.log("first day of the week");
+        break
+    case "Tuesday":
+        console.log("Second day of the week");
+    default:
+        console.log("No days off")
+}
+
+*/
