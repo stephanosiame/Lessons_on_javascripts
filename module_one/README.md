74300af792bc1ebf3f0a6eeec7ecacd8692aa8ca
stephanosiame <siamelstephano@gmail.com>
2025-07-25 10:34:21 +0300

    lesson 1 about variables

---
+++ module_one/README.md
@@ -0,0 +1,7 @@
+## Roles of Programming
+
+- Programming is the sets of instructions that is enable to perform a cartain functions 
+JavaScript is the one of the programming language that carry instructions so that to perform the certain instructions to solve a problem.
+
+Role of JavaScript
+- 
---
+++ module_one/Variables/lesson_variable.js
@@ -0,0 +1,18 @@
+// variable is like container that used to keep values that may used frequent within the program.
+//javascript have the several ways for the naming variable there three most used case for the name the variable
+/*
+- Use of "Var" this was first introduced and most usefull is daynamic
+- Use of "let" This is the modern used to name the variable that can change but is specific target and scope
+- Use of "Const" This is used to keeep the value which is costant which can not change within the program
+*/
+
+//There two words that is used in programming. which is  decleare and initialization 
+
+//examples:
+
+var person; // declare the variable person
+var person = "Asha Jumbe" //initialize the variable
+
+let name = "Ashura"
+
+const pie = 3.14
