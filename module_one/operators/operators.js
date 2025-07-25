b42e520fc618fd4c78be338b33d37b6527be3457
stephanosiame <siamelstephano@gmail.com>
2025-07-25 13:35:33 +0300

    lesson 3 operator in js

---
+++ module_one/operators/operators.js
@@ -0,0 +1,118 @@
+//JavaScript have the different operators that is facillitate the different operations within the program 
+/*
+Assignment Oparetors " = "
+
+| Operator | Meaning             | Example                |
+| -------- | ------------------- | ---------------------- |
+| `=`      | Assign              | `x = 10`               |
+| `+=`     | Add and assign      | `x += 5` → `x = x + 5` |
+| `-=`     | Subtract and assign | `x -= 5`               |
+| `*=`     | Multiply and assign | `x *= 5`               |
+| `/=`     | Divide and assign   | `x /= 5`               |
+| `%=`     | Modulus and assign  | `x %= 5`               |
+
+
+used to assign variables and also can support operations of the arthimetic operations 
+*/
+
+/*
+Arthmetic Oparetors
+
+additions: +
+Substruction : -
+devision: /
+Multiplication: *
+*/
+
+/*
+Comparisson operators
+
+| Operator | Meaning               | Example     | Result  |
+| -------- | --------------------- | ----------- | ------- |
+| `==`     | Equal (loose)         | `5 == "5"`  | `true`  |
+| `===`    | Equal (strict)        | `5 === "5"` | `false` |
+| `!=`     | Not equal (loose)     | `5 != "5"`  | `false` |
+| `!==`    | Not equal (strict)    | `5 !== "5"` | `true`  |
+| `>`      | Greater than          | `5 > 2`     | `true`  |
+| `<`      | Less than             | `5 < 2`     | `false` |
+| `>=`     | Greater than or equal | `5 >= 5`    | `true`  |
+| `<=`     | Less than or equal    | `5 <= 4`    | `false` |
+
+*/
+
+/*
+Logical Operators
+
+| Operator | Name | Example         | Result  |     
+| -------- | ---- | --------------- | ------- | 
+| `&&`     | AND  | `true && false` | `false` |        
+| `        |      | `               | OR      | 
+| `!`      | NOT  | `!true`         | `false` |       
+
+*/
+
+
+/*
+
+String Oparetor
+
+| Operator | Use         | Example              | Result          |
+| -------- | ----------- | -------------------- | --------------- |
+| `+`      | Concatenate | `"Hello " + "World"` | `"Hello World"` |
+| `+=`     | Append      | `msg += "!"`         | `"Hello!"`      |
+
+*/
+
+
+/*
+
+Type operator  useful in debbuging and validation
+
+| Operator     | Purpose            | Example                | Result     |
+| ------------ | ------------------ | ---------------------- | ---------- |
+| `typeof`     | Type of operand    | `typeof 123`           | `'number'` |
+| `instanceof` | Checks object type | `obj instanceof Array` | `true`     |
+
+
+*/
+
+/*
+
+Bitwise operator
+
+| Operator | Name        | Use           | 
+| -------- | ----------- | ------------- | 
+| `&`      | AND         | `5 & 1 → 1`   |  
+| \`       | \`          | OR            | 
+| `^`      | XOR         | `5 ^ 1 → 4`   |     
+| `~`      | NOT         | `~5 → -6`     |     
+| `<<`     | Left shift  | `5 << 1 → 10` |     
+| `>>`     | Right shift | `5 >> 1 → 2`  |     
+
+*/
+
+
+
+/*
+
+Ternary Operator (? :)
+
+let result = age >= 18 ? "Adult" : "Minor";
+
+*/
+
+/*
+
+Nullish Coalescing Operator (??)
+
+let name = userName ?? "Guest";
+
+*/
+
+/*
+
+Optional Chaining (?.)
+
+let city = user?.address?.city;
+
+*/
