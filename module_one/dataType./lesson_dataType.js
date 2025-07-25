c623560cee985be14ac57a3d2151252ba2d18c9c
stephanosiame <siamelstephano@gmail.com>
2025-07-25 12:58:05 +0300

    lesson 2 about Data Type

---
+++ module_one/dataType./lesson_dataType.js
@@ -0,0 +1,36 @@
+//Data type is among of the lesson important to  the javascript and other programming language that is use as identinty of the value that is provided to the program
+//There two type of categories of the javascript datatype which are Primetive dataType, and non-Primitive data type
+
+/*
+Primitive data type meaning and its types and reason of why it call primitive data type and how it used
+
+why it called primitive data type : -They store single, simple values
+
+-They are immutable (cannot be changed once created)
+
+-They are not objects and have no methods or properties
+
+-They are stored directly in memory by value
+
+Types of primitive datatype
+1. Number example let age = 22
+2. String example let name = "stephano"
+3. Boolean example let paid = true, let send= false
+4. Null example let selected = null;
+5. Undefined example  let data; console.log(data);
+6. Symbols (ES6+) example let sym = symbol("id");
+7. BigInt example let bigInt = 1234657687487839274773673678287327632673827572863235364622237647847676276367375463672n;
+
+*/
+
+/*
+Non-Primitive Data Type
+This is objects that hold the collections and complex entities 
+examples of non-primitive data type are 
+
+1. Function
+2. arrays
+3. Objects
+4. built-in objects type 
+
+*/
