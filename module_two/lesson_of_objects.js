03551c3476441f6d35e8cc555936d6c2053dcee6
stephanosiame <siamelstephano@gmail.com>
2025-07-27 10:02:41 +0300

    add lesson3 of module 2

---
+++ module_two/lesson_of_objects.js
@@ -0,0 +1,26 @@
+//object is the way to store the related access of the value and properties within the program and mist case have value and properies that is pessesed so that to endure all program can excute it 
+//object notation this is the way in which can enable to retrive and access the object within the project.
+/*
+Math.random()
+
+var decimal = Math.random();
+
+console.log(decimal)
+
+console.log(decimal * 10)
+*/
+
+/*
+Math.ceil()
+
+var round = Math.ceil(1.1)
+
+console.log(round)
+*/
+
+Math.random()
+var decimal= Math.random()
+
+var round = Math.ceil(decimal)
+
+console.log(round)
