169ebbd717efce8e400c825516d8233dbc691a20
stephanosiame <siamelstephano@gmail.com>
2025-07-27 11:06:29 +0300

    add lesson 4 of module 2 string

---
+++ module_two/lesson_of_string.js
@@ -0,0 +1,9 @@
+//String is the sequence of characters that placed bettewn qoutes can be either single quates or double quates
+
+//String also can be iteratable like arrays
+
+var name = "stephano"
+
+for (i=0; i <= name.length; i ++){
+    console.log(name[i]);
+}
