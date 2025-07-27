7a7a9997aab3858f36127a032a6ac681171f53f8
stephanosiame <siamelstephano@gmail.com>
2025-07-27 09:30:48 +0300

    add lesson 2 in module 2 array

---
+++ module_two/Exercisise/area_of_rectangle.js
@@ -0,0 +1,4 @@
+function areaOfRectangle(length, width){
+    return length * width
+}
+console.log(areaOfRectangle(5, 2) + "cm square")
---
+++ module_two/array.js
@@ -0,0 +1,6 @@
+//The array in programmig is used to  like place order for the items within the program that used to keep data in list form 
+// array use the [] square bracket is identify array type.
+
+
+var carType = ["Toyota", "RangeOver", "BMW", "Ford Mustang", 20]
+console.log(carType[3])
